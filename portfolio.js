7d7dc90995d1e907ef2748331dd692ab567ad49c
Vandana Sharma <vs4160319@gmail.com>
2024-05-24 13:08:39 +0530

    Adding my portfolio

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname){
+//    for(tablink of tablinks){
+//        tablink.classList.remove("active-link")
+//    }
+//    for(tabcontent of tabcontents){
+//        tablink.classList.remove("active-tab");
+//    }
+//    Event.currentTarget.classListadd("active-link");
+//    document.getElementsById(tabname). classList.add("active-tab");
+// }
+
+
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+sidemeu.style.right="0";
+}
+
+function closemenu(){
+sidemeu.style.right= "-200px"
+}
